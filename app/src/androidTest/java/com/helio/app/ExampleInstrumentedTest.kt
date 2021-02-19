18fa42796352399a8150b0c0218fae32d4fc915a
Adam <adamb56789@gmail.com>
2021-02-19 19:26:05 +0000

    Added instrumented tests (which don't work)

---
+++ app/src/androidTest/java/com/helio/app/ExampleInstrumentedTest.kt
@@ -0,0 +1,68 @@
+package com.helio.app
+
+import android.view.View
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.BoundedMatcher
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.android.material.slider.Slider
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Rule
+    var activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun controlSliderWorks() {
+        // https://stackoverflow.com/a/65400816/
+        onView(withId(R.id.controlSlider)).check(matches(withValue(1.0F)))
+
+        onView(withId(R.id.controlSlider)).perform(setValue(2.0F))
+
+        onView(withId(R.id.controlSlider)).check(matches(withValue(2.0F)))
+    }
+
+    private fun withValue(expectedValue: Float): Matcher<View?> {
+        return object : BoundedMatcher<View?, Slider>(Slider::class.java) {
+            override fun describeTo(description: Description) {
+                description.appendText("expected: $expectedValue")
+            }
+
+            override fun matchesSafely(slider: Slider?): Boolean {
+                return slider?.value == expectedValue
+            }
+        }
+    }
+
+    fun setValue(value: Float): ViewAction {
+        return object : ViewAction {
+            override fun getDescription(): String {
+                return "Set Slider value to $value"
+            }
+
+            override fun getConstraints(): Matcher<View> {
+                return ViewMatchers.isAssignableFrom(Slider::class.java)
+            }
+
+            override fun perform(uiController: UiController?, view: View) {
+                val seekBar = view as Slider
+                seekBar.value = value
+            }
+        }
+    }
+}
